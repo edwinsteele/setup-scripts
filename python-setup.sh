2634c70ca5145edffb0f8c6fd47c9132c16b48c3
Edwin Steele <edwin-lists@wordspeak.org>
2012-10-16 07:06:16 +1100

    A re-write to support linux and macos

--- python-setup.sh
+++ python-setup.sh
@@ -6,61 +6,89 @@ fi
 
 PYTHON_VERSION="2.7"
 
-which python$PYTHON_VERSION
+STARTING_DIR=$(pwd)
+
+if [ $(uname) == "Darwin" ];
+then
+	LOCAL_SITE_BIN=~/Library/Python/$PYTHON_VERSION/bin
+	LOCAL_SITE_LIB=~/Library/Python/$PYTHON_VERSION/lib/python/site-packages
+elif [ $(uname) == "Linux" ];
+then
+	LOCAL_SITE_BIN=~/.local/bin
+	LOCAL_SITE_LIB=~/.local/lib/python$PYTHON_VERSION/site-packages
+
+else
+	echo "Don't know how to run on this platform: $(uname)";
+	exit 1;
+fi
+
+BOOTSTRAP_DIR=$LOCAL_SITE_BIN/../bootstrap
+
+which python$PYTHON_VERSION > /dev/null
 if [ $? -ne 0 ];
 then
   echo "python interpreter (version $PYTHON_VERSION) not in path. exiting";
   exit 1;
 fi
 
-echo "Make sure ~/.local/bin is at the front of your PATH"
-echo "Make sure ~/.local is on PYTHONPATH"
+mkdir -p $LOCAL_SITE_BIN
+mkdir -p $LOCAL_SITE_LIB
+mkdir -p $BOOTSTRAP_DIR
 
-mkdir -p ~/.local/lib/python$PYTHON_VERSION/site-packages
-cat > ~/.pydistutils.cfg << EOF
-[install]
-prefix=~/.local
+cd $BOOTSTRAP_DIR
+curl -O http://python-distribute.org/distribute_setup.py
+curl -O http://pypi.python.org/packages/source/p/pip/pip-1.2.1.tar.gz
+tar -zxf $BOOTSTRAP_DIR/pip-1.2.1.tar.gz
+
+cd $BOOTSTRAP_DIR
+python$PYTHON_VERSION distribute_setup.py --user
+cd $BOOTSTRAP_DIR/pip-1.2.1
+python$PYTHON_VERSION setup.py install --user
+
+$LOCAL_SITE_BIN/pip-$PYTHON_VERSION install --user virtualenv
+$LOCAL_SITE_BIN/pip-$PYTHON_VERSION install --user virtualenvwrapper
+
+PYTHON_PROFILE_SCRIPT=~/.python_profile_additions_$PYTHON_VERSION.sh
+
+cat > $PYTHON_PROFILE_SCRIPT << EOF
+export PATH=$LOCAL_SITE_BIN:\$PATH
+alias pip$PYTHON_VERSION="$LOCAL_SITE_BIN/pip-$PYTHON_VERSION"
+alias install$PYTHON_VERSION="pip$PYTHON_VERSION install --user"
+
 EOF
 
-# Pinched from http://stackoverflow.com/questions/4324558/whats-the-proper-way-to-install-pip-virtualenv-and-distribute-for-python
-curl --output ~/distribute_setup.py http://python-distribute.org/distribute_setup.py
-python$PYTHON_VERSION /tmp/distribute_setup.py
-rm ~/distribute_setup.py
-# use easy install because we don't have pip yet, then revert to pip
-easy_install pip
-pip install virtualenv
-pip install virtualenvwrapper
-mkdir ~/.virtualenvs
+VENV_PROFILE_SCRIPT=~/.virtualenvs/venv_profile_additions_$PYTHON_VERSION.sh
+cat > $VENV_PROFILE_SCRIPT << EOF
+VENV_DIR=$HOME/.virtualenvs
 
-cat > ~/.virtualenvs/venv_profile_additions.sh << EOF
-VENV_DIR=$HOME/.virtualEnvs
+export VIRTUALENVWRAPPER_PYTHON=$(which python$PYTHON_VERSION)
+export VIRTUALENVWRAPPER_VIRTUALENV=$LOCAL_SITE_BIN/virtualenv-$PYTHON_VERSION
+export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages --distribute'
+export WORKON_HOME=\$VENV_DIR
 
 # Locate virtualenvwrapper binary
-if [ -f ~/.local/bin/virtualenvwrapper.sh ]; then
-    export VENVWRAP=~/.local/bin/virtualenvwrapper.sh
+if [ -f $LOCAL_SITE_BIN/virtualenvwrapper.sh ]; then
+    export VENVWRAP=$LOCAL_SITE_BIN/virtualenvwrapper.sh
 fi
 
-if [ ! -z $VENVWRAP ]; then
-    # virtualenvwrapper -------------------------------------------
-    # make sure env directory exists; else create it
-    [ -d $VENV_DIR ] || mkdir -p $VENV_DIR
+if [ ! -z \$VENVWRAP ]; then
+    [ -d \$VENV_DIR ] || mkdir -p \$VENV_DIR
     export WORKON_HOME=$VENV_DIR
-    source $VENVWRAP
-
-    # virtualenv --------------------------------------------------
+    source \$VENVWRAP
     export VIRTUALENV_USE_DISTRIBUTE=true
-
     # pip ---------------------------------------------------------
-    export PIP_VIRTUALENV_BASE=$WORKON_HOME
+    export PIP_VIRTUALENV_BASE=\$WORKON_HOME
     export PIP_REQUIRE_VIRTUALENV=true
     export PIP_RESPECT_VIRTUALENV=true
     export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
 fi
 EOF
 
-echo "Make the following changes to ~/.bash_profile"
-echo "Add ~/.local/bin to the start of PATH and export it (it needs to come before /opt/python2.7/bin)"
-echo "Add: source ~/.virtualEnvs/venv_profile_additions.sh"
-#test -d ~/.local || mkdir ~/.local/lib/python2.7/site-packages;
-#export PATH=~/.local/bin:/opt/python2.7/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
-#alias python=python2.7
+echo "##############################"
+echo "Add the following lines to ~/.bash_profile:"
+#echo "Add $LOCAL_SITE_BIN to the start of PATH and export it (it needs to come before /opt/python2.7/bin)"
+echo "source $PYTHON_PROFILE_SCRIPT"
+echo "source $VENV_PROFILE_SCRIPT"
+
+# Finish where we started
+cd $STARTING_DIR
