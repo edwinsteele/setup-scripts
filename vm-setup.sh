bbebeea470beb65fd4beb1705c3e4440ba2abf61
Edwin Steele <edwin-lists@wordspeak.org>
2012-10-20 16:57:04 +1100

    Use mktemp for temp directory. Don't try to build shared lib support for 3.3 as it doesn't work. fix untar comands

--- vm-setup.sh
+++ vm-setup.sh
@@ -10,11 +10,11 @@ fi
 yum groupinstall "development tools" -y
 yum install readline-devel openssl-devel gmp-devel ncurses-devel gdbm-devel zlib-devel expat-devel libGL-devel tk tix gcc-c++ libX11-devel glibc-devel bzip2 tar tcl-devel tk-devel pkgconfig tix-devel bzip2-devel sqlite-devel autoconf db4-devel libffi-devel valgrind-devel -y
 
-mkdir tmp
+t=$(mktemp -d)
 # Build 2.7.2
-cd tmp
+cd $t
 curl -O http://python.org/ftp/python/2.7.2/Python-2.7.2.tar.bz2
-tar xfj Python-2.7.2.tgz
+tar xfj Python-2.7.2.tar.bz2
 cd Python-2.7.2
 ./configure --prefix=/opt/python2.7 --enable-shared
 make
@@ -22,20 +22,18 @@ make altinstall
 echo "/opt/python2.7/lib" >> /etc/ld.so.conf.d/opt-python2.7.conf
 
 # Build 3.3.0
-cd tmp
+cd $t
 curl -O http://www.python.org/ftp/python/3.3.0/Python-3.3.0.tar.bz2
 tar xfj Python-3.3.0.tar.bz2
 cd Python-3.3.0
-./configure --prefix=/opt/python3.3 --enabled-shared
+./configure --prefix=/opt/python3.3
 make
 make altinstall
 echo "/opt/python3.3/lib" >> /etc/ld.so.conf.d/opt-python3.3.conf
 
 
 ldconfig
-cd ..
-cd ..
-rm -rf tmp
+rm -rf $t
 
 # End python build
 
