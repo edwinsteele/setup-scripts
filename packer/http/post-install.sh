567c4628cc314a80251c28efa47425322cd11457
Edwin Steele <edwin-lists@wordspeak.org>
2016-06-18 07:06:13 +1000

    Use keys to connect, even from first start after install

--- packer/http/post-install.sh
+++ packer/http/post-install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-echo "Mirror is " $1
-ftp -o /SHA256 $1/pub/OpenBSD/5.9/SHA256
-echo "hello";
+# Corresponds to the ssh_private_key_file directive in the packer template
+ROOT_PUBLIC_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZOQQUwe79XugZrvnJNIgEglmzVa0h+/TgEaI534WA83j0Q56HcE97Ma47EZ3tZMJYDr5w81iXFga5P0ciED8ieaBEaMKES028rT10uLuZkoLx+jOTgOdHfuEzudcTrM4XTF+7oJJg/Y4mA6ltajgutoMKHDc4LHb8iafIVgnxZlaZTMQa/pJ3olVA/r6eK/EsGiqxRPjWGulG9pfr/0oxWZiwaf1IwW9HmtqHAzC8nyulBhvzF+iRrhBvuOkpinLS51QaUPddRC1msiP9TVDM66wbQL4Iz8tYx8mopDHCR7a3JqlsfK4KqM3nvxL+11j087nGHVwmFRhZ7Zun7LfV esteele@mercury.local"
+mkdir /root/.ssh
+chmod 700 /root/.ssh
+echo $ROOT_PUBLIC_KEY > /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
